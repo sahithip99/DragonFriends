2f87ada7693da1588454003b80bb196252d732ee
vyoung708 <vyoung708@yahoo.com>
2018-04-22 23:09:13 -0400

    no message

--- team47-dragonfriends/app/src/main/java/com/drexel/team47/dragonfriends/SearchActivity.java
+++ team47-dragonfriends/app/src/main/java/com/drexel/team47/dragonfriends/SearchActivity.java
@@ -1,32 +1,67 @@
 package com.drexel.team47.dragonfriends;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 //import com.google.auth.oauth2.GoogleCredentials;
 
 import com.loopj.android.http.*;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.Dictionary;
 
 import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private TextView className;
+    private TextView secNum;
+    private TextView crnNum;
+    private TextView classType;
+    private TextView instructor;
+    private TextView dayTime;
+
+
+    private FloatingActionButton addClass;
+
     private SearchView classSearch;
 
+    private JSONObject res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         classSearch = (SearchView) findViewById(R.id.classSearchView);
+
+        className = (TextView) findViewById(R.id.class_name);
+        secNum = (TextView) findViewById(R.id.sec_num);
+        crnNum = (TextView) findViewById(R.id.crn_num);
+        classType = (TextView) findViewById(R.id.instr_type);
+        instructor = (TextView) findViewById(R.id.instr_name);
+        dayTime = (TextView) findViewById(R.id.day_time);
+
+        addClass = (FloatingActionButton) findViewById(R.id.add_class);
+
+        addClass.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                System.out.println("Added class " + res);
+                //replace with the backend function to add the class to the user's info in database
+            }
+
+
+        });
 
         //Listen to query text
         classSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -40,17 +75,17 @@ public class SearchActivity extends AppCompatActivity {
                 //Add parameters for POST request
                 RequestParams params = new RequestParams("crn", s);
                 System.out.println("logging");
-                String url = "https://risum.serveo.net/classByCrn";
+                String url = "https://dragonfriends-eb4fc.firebaseapp.com/classByCrn";
 
                 client.post(url, params, new AsyncHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        System.out.println("Successssss");
+
                         try {
 
-                            JSONObject res= new JSONObject(new String(responseBody));
+                            res= new JSONObject(new String(responseBody));
                             System.out.println(res);
-
+                            displayData(res);
 
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -79,7 +114,36 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * private TextView className;
+     private TextView secNum;
+     private TextView crnNum;
+     private TextView classType;
+     private TextView instructor;
+     private TextView dayTime;
+     * @param info
+     */
+    private void displayData(JSONObject info){
+        try {
+            String name = info.getString("courseTitle");
+            String sec = info.getString("sec");
+            String crnN = info.getString("crn");
+            String classT = info.getString("instrType");
+            String instr = info.getString("instructor");
+            String dayT = info.getString("dayTime");
+            className.setText(name);
+            secNum.setText(sec);
+            crnNum.setText(crnN);
+            classType.setText(classT);
+            instructor.setText(instr);
+            dayTime.setText(dayT);
+            addClass.setVisibility(View.VISIBLE);
+            addClass.setClickable(true);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
+    }
 
 }
